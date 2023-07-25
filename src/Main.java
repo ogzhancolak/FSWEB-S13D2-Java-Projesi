75b50c38da8ae3360acc14a3f0025eacdb52776d
ogzhancolak <ogzhancolak@outlook.com>
2023-07-25 11:24:49 +0300

    son soru yapılacak

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println(isPalindrone(122));
+        System.out.println(isPerfectNumber(6));
     }
+
+    public static boolean isPalindrone(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number > 0) {
+            int lastDigit = number % 10;
+            reversedNumber = reversedNumber * 10 + lastDigit;
+            number /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if(number <= 0) {
+            return false;
+        }
+
+        int sumOfdiv = 0;
+        for(int i = 1; i <= number / 2; i++) {
+            if(number % i == 0) {
+                sumOfdiv += i;
+            }
+        }
+
+        return sumOfdiv == number;
+    }
+
+
 }
